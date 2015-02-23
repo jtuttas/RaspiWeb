49b6f671250c1220a4abf314c0494d0d8b723e20
jtuttas <tuttas@mmbbs.de>
2015-02-23 17:14:49 +0100

    init2

---
+++ raspiTest/src/de/raspi/LEDControl.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.raspi;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jörg
+ */
+public class LEDControl implements Runnable{
+
+    // create gpio controller
+    private GpioController gpio;
+    // provision gpio pin #01 as an output pin and turn on
+    private GpioPinDigitalOutput opin;
+    private long onTime=0;
+    private Thread dimmer;
+    private boolean running=false;
+    private static LEDControl instance;
+
+    private LEDControl() {
+        this(RaspiPin.GPIO_01, PinState.HIGH);
+    }
+    
+    private LEDControl(Pin pin, PinState state) {
+        gpio = GpioFactory.getInstance();
+        opin = gpio.provisionDigitalOutputPin(pin, state);
+        if (state==PinState.HIGH) this.dim(100);
+        else this.dim(0);
+        dimmer = new Thread(this);
+        running=true;
+        dimmer.start();
+    }
+
+    public static LEDControl getInstance(Pin pin, PinState state) {
+        if (instance==null) {
+            instance=new LEDControl(pin, state);
+        }
+        return instance;
+    }
+    
+    public void turnOn(boolean b) {
+        if (b) {
+            this.dim(100);
+        } else {
+            this.dim(0);
+
+        }
+    }
+
+    public void shutDown() {
+        running = false;
+        gpio.shutdown();
+    }
+
+    public void dim(int i) {
+        onTime=20*i/100;
+        System.out.println ("Dimmer set to "+i+"% onTime is "+onTime+" ms");
+    }
+
+    
+
+    @Override
+    public void run() {
+        //TODO auto generated Method
+        while(running) {
+            opin.high();
+            try {
+                Thread.sleep(onTime);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(LEDControl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            opin.low();
+            try {
+                Thread.sleep(20-onTime);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(LEDControl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+    }
+}
---
+++ raspiTest/src/de/raspi/RaspiTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.raspi;
+
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jörg
+ */
+public class RaspiTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LEDControl ledControl = LEDControl.getInstance(RaspiPin.GPIO_01, PinState.HIGH);
+        System.out.println("<--Pi4J--> GPIO Control Example ... started.");
+        
+        
+        // provision gpio pin #01 as an output pin and turn on
+        System.out.println("--> GPIO state should be: ON");
+        
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(RaspiTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        // turn off gpio pin #01
+        ledControl.turnOn(false);
+        System.out.println("--> GPIO state should be: OFF");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(RaspiTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        ledControl.dim(50);
+        System.out.println("--> GPIO state should be: 50%");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(RaspiTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        ledControl.shutDown();
+
+    }
+    
+}
